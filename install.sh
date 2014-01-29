084af81900e12af1364ecadf94564ded25f0bc14
caducas <nikolaus@famkofler.com>
2014-01-29 13:53:35 +0100

    added package file to install duino with npm modified install.sh to install duino over npm and copy lib folder from current duino github repo

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo 'include ../plugins/Arduino/views/eventListener' >> ../../views/eventListener.jade
+
+npm install
+
+git clone https://github.com/ecto/duino
+cp -r duino/lib/ node_modules/duino/
+
+rm -rf duino
